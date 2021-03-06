2e6909a3cc09ba76aafc0083802d8663ee01e2f2
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-06 21:39:46 +0200

    center content

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,13 +5,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 const components = [
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
 ];
 
 @NgModule({
