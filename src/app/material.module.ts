6ca187fe4439a84556f469f9f70ce370aef33529
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-06 18:47:47 +0200

    added toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 const components = [
-
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
 ];
 
 @NgModule({
-  declarations: [
-  ],
   imports: components,
   exports: components,
   providers: [],
