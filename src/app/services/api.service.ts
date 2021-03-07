b83c73575ab0b463755f0ce69821d4907028be6e
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-07 15:11:07 +0200

    http

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,19 +16,19 @@ export class ApiService implements IapiService {
   constructor(private http: HttpClient) { }
 
   getLocations(query: string): Observable<Location[]> {
-    return this.http.get<Location[]>(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${encodeURIComponent(query)}`).pipe(
+    return this.http.get<Location[]>(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${encodeURIComponent(query)}`).pipe(
       catchError(() => of([]))
     )
   }
 
   getCurrentConditions(key: string): Observable<CurrentConditions[]> {
-    return this.http.get<CurrentConditions[]>(`https://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${API_KEY}`).pipe(
+    return this.http.get<CurrentConditions[]>(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${API_KEY}`).pipe(
       catchError(() => of([]))
     )
   }
 
   getForecasts(key: string): Observable<Forecast[]> {
-    return this.http.get<ForcastHttpResponse>(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${API_KEY}&metric=true`).pipe(
+    return this.http.get<ForcastHttpResponse>(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${API_KEY}&metric=true`).pipe(
       map(res => res.DailyForecasts),
       catchError(() => of([]))
     )
