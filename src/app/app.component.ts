bf3579efdc487d6281828f2aa594dc52ceceaaca
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-08 22:27:20 +0200

    http loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FavoriteLocationsQuery } from './store/favorite-locations/state/favorite-locations.query';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'herolo-weather';
+  isLoading$: Observable<boolean>;
+
+  constructor(private favoriteLocationsQuery: FavoriteLocationsQuery) {}
+
+  ngOnInit(): void {
+    this.isLoading$ = this.favoriteLocationsQuery.selectLoading();
+  }
+
+
 }
