6eedd64540793d8a201fc58e5d1d6a312f1d9938
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-09 08:45:48 +0200

    api

--- src/app/services/api,interface.ts
+++ src/app/services/api,interface.ts
@@ -3,7 +3,7 @@ import { CurrentConditions } from "../model/current-conditions";
 import { Forecast, ForecastHttpResponse } from "../model/forecast";
 import { Location } from "../model/location";
 
-export const API_KEY = 'gRf4KNnswLuVm8mG3puAI1GUOGeJTu1v';
+export const API_KEY = 'Lx1f7HdveztpydjGREeVy3VPdhwWK4iK';
 
 export interface IapiService {
     getLocations(query: string): Observable<Location[]>;
