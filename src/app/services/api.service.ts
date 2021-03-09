354ddfb6205aefba97ea5cc9d7d57923a8a3c5f4
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-09 07:45:11 +0200

    handle error

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
+import { AppComponent } from './../app.component';
 import { API_KEY, IapiService } from './api,interface';
-import { Observable, of } from 'rxjs';
-import { Injectable } from '@angular/core';
+import { EMPTY, Observable, of } from 'rxjs';
+import { Injectable, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Location } from "../model/location";
 import { CurrentConditions } from "../model/current-conditions";
@@ -16,31 +17,29 @@ export class ApiService implements IapiService {
 
   HTTP_PREFIX = 'https://cors-anywhere.herokuapp.com/';
 
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {}
 
   getLocations(query: string): Observable<Location[]> {
     return this.http.get<Location[]>(`${this.HTTP_PREFIX}http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${encodeURIComponent(query)}`).pipe(
-      catchError(this.handleError)
+      catchError(this.handleError.bind(this))
     )
   }
 
   getCurrentConditions(key: string): Observable<CurrentConditions[]> {
     return this.http.get<CurrentConditions[]>(`${this.HTTP_PREFIX}http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${API_KEY}`).pipe(
-      catchError(this.handleError)
+      catchError(this.handleError.bind(this))
     )
   }
 
   getForecasts(key: string): Observable<ForecastHttpResponse[]> {
     return this.http.get<ForecastsHttpResponse>(`${this.HTTP_PREFIX}http://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${API_KEY}&metric=true`).pipe(
       map(res => res.DailyForecasts),
-      catchError(this.handleError)
+      catchError(this.handleError.bind(this))
     )
   }
 
   handleError(): Observable<any[]> {
-    this._snackBar.open('Bad request', '', {
-      duration: 2000,
-    });
-    return of([]);
+    this._snackBar.open('Bad request', '', { duration: 2000 });
+    return EMPTY;
   }
 }
