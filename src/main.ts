4f99321f7df723af5bf04b698839df4e8302f578
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-17 19:02:52 +0300

    persist state

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { persistState } from '@datorama/akita';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const storage = persistState();
+
+const providers = [{ provide: 'persistStorage', useValue: storage }];
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.error(err));
