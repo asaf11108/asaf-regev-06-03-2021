cc90837ba31ee26a3bc6d2a63ce3051714ed2fb2
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-17 18:46:47 +0300

    reduce rendering in pipe

--- src/app/pipes/temperature.pipe.ts
+++ src/app/pipes/temperature.pipe.ts
@@ -10,12 +10,22 @@ import { Subscription } from 'rxjs';
 })
 export class TemperaturePipe implements PipeTransform, OnDestroy {
   private subscription: Subscription | null = null;
-  private lastValue: string = '';
+  private lastTemperature: number | null = null;
+  private lastValue = '';
 
   constructor(private cdr: ChangeDetectorRef, private globalQuery: GlobalQuery) {}
 
+  // Reverse engineering from Transloco pipe
+  // https://github.com/ngneat/transloco/blob/10c4e25cfdac4f56ecd8dabd6efeed3e08126476/projects/ngneat/transloco/src/lib/transloco.pipe.ts#L64
   transform(temperature: number): string {
-    this.subscription && this.subscription.unsubscribe();
+    // This pipe is impure, so the following code block prevent from running the entire function
+    if (temperature === this.lastTemperature) {
+      return this.lastValue;
+    }
+    this.lastTemperature = temperature;
+    // --------------------------------------------
+
+    this.subscription && this.subscription.unsubscribe(); // Temperature is premitive value and update Value will run old value (closure)
 
     this.subscription = this.globalQuery.temperatureType$.subscribe(temperatureType => this.updateValue(temperature, temperatureType))
 
@@ -34,7 +44,7 @@ export class TemperaturePipe implements PipeTransform, OnDestroy {
 
     this.lastValue = `${temperature}\u00B0${TemperatureTypeSymbol[temperatureType]}`;
 
-    this.cdr.markForCheck();
+    this.cdr.markForCheck(); // Trigger change detection inside onPush component
   }
 
 }
