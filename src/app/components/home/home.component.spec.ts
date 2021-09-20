b93a79b5ed44f38b921f46a47c0530aa1992bcb0
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-09-20 16:14:25 +0300

    testing init

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApiService as ApiMockService } from 'src/app/services/api.mock.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from 'src/app/services/api.mock.service';
+import { WeatherLocationsQuery } from 'src/app/state/weather-locations/weather-locations.query';
+import { WeatherLocationsService } from 'src/app/state/weather-locations/weather-locations.service';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: HomeComponent;
+  let weatherLocationsQuery: WeatherLocationsQuery;
+  let weatherLocationsService: WeatherLocationsService;
+  let apiService: ApiService;
+  let snackBar: MatSnackBar;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = new ApiMockService();
+
+    fixture = new HomeComponent(
+      weatherLocationsQuery,
+      weatherLocationsService,
+      apiService,
+      snackBar
+    );
+    console.log(
+      '🚀 ~ file: home.component.spec.ts ~ line 29 ~ describe ~ fixture',
+      fixture
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    console.log(fixture.weatherLocation$.value);
   });
+
+  describe('ngOnInit', () => {});
+
+  it('should create', () => {});
 });
